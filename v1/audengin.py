e54a6a668bc6c463e30a56fe58fec9cb8c1ce7c9
FUNCODE <89914933+SUBOdhar@users.noreply.github.com>
2024-01-14 09:02:49 +0545

    new sound model

---
+++ v1/audengin.py
@@ -0,0 +1,3 @@
+from audsrc import *
+# Make sure both audio clips have the same length
+combined_audio = audio7 + audio13
---
+++ v1/audio.py
@@ -0,0 +1,31 @@
+import pygame
+from audsrc import *
+from audengin import *
+nepali_letters = ['अ', 'आ', 'इ', 'ई', 'उ', 'ऊ', 'ऋ', 'ए', 'ऐ', 'ओ', 'औ', 'अं', 'अः', 'क', 'ख', 'ग', 'घ', 'ङ', 'च', 'छ', 'ज',
+                  'झ', 'ञ', 'ट', 'ठ', 'ड', 'ढ', 'ण', 'त', 'थ', 'द', 'ध', 'न', 'प', 'फ', 'ब', 'भ', 'म', 'य', 'र', 'ल', 'व', 'श', 'ष', 'स', 'ह']
+
+
+
+
+# Export the combined audio to a temporary WAV file
+combined_audio.export(
+    r"C:\Users\aryal\OneDrive\Desktop\voice model\v1\temp\combined_temp.wav", format="wav")
+
+
+# Initialize pygame
+pygame.init()
+
+# Load the combined audio
+pygame.mixer.init()
+pygame.mixer.music.load(
+    r"C:\Users\aryal\OneDrive\Desktop\voice model\v1\temp\combined_temp.wav")
+
+# Play the combined audio
+pygame.mixer.music.play()
+
+# Wait for the sound to finish playing
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
+
+# Clean up pygame
+pygame.quit()
